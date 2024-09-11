420694fd6cd511bb0ee7a2e68448803f3b2722c9
shochcho <shochcho46@gmail.com>
2024-09-12 03:15:39 +0600

    start developing

---
+++ resources/views/frontend/layout/meta.blade.php
@@ -0,0 +1,10 @@
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>{{ config('app.name', 'Laravel') }}</title>
+<meta name="csrf-token" content="{{ csrf_token() }}">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="author" content="ColorlibHQ">
+<meta name="description" content="DewanEnterprise provides reliable Broadband services with advanced features, offering seamless internet solutions for homes and businesses.">
+<meta name="keywords" content="broadband services, internet solutions, home broadband, business broadband, high-speed internet, DewanEnterprise, broadband provider, fiber internet, DewanEnterprise broadband, DewanEnterprise internet services, WiFi solutions, broadband plans, ISP">
+
+<title>DewanEnterprise</title>
